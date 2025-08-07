c9af0e397582a3239c9d87ed0c287e1a135ee647
Salil Joshi <97686813+salil-sj@users.noreply.github.com>
2025-08-07 17:05:12 +0530

    Update UpdateSLider.tsx

--- UpdateSLider.tsx
+++ UpdateSLider.tsx
@@ -5,34 +5,11 @@ const handleSliderChange = useCallback(
     onChange({ value: newGlobalValue });
 
     if (!isDraggingRef.current) {
-      const lowerShiftPoint = windowStart + windowSize * (1 - shiftThreshold);
-      const upperShiftPoint = windowStart + windowSize * shiftThreshold;
-
-      if (newGlobalValue < windowStart + windowSize * shiftThreshold && currentWindow > 0) {
-        // Value moved close to start of window -> consider going to previous window
-        const newWindow = getWindowForValue(newGlobalValue);
-        if (newWindow !== currentWindow && newWindow < currentWindow) {
-          scheduleWindowShift(newWindow);
-        }
-      } else if (newGlobalValue > windowStart + windowSize * (1 - shiftThreshold) && currentWindow < maxWindow) {
-        // Value moved close to end of window -> consider going to next window
-        const newWindow = getWindowForValue(newGlobalValue);
-        if (newWindow !== currentWindow && newWindow > currentWindow) {
-          scheduleWindowShift(newWindow);
-        }
+      const newWindow = getWindowForValue(newGlobalValue);
+      if (newWindow !== currentWindow) {
+        scheduleWindowShift(newWindow);
       }
     }
   },
-  [
-    minValue,
-    maxValue,
-    onChange,
-    currentWindow,
-    windowStart,
-    windowSize,
-    shiftThreshold,
-    scheduleWindowShift,
-    maxWindow,
-    getWindowForValue
-  ]
+  [minValue, maxValue, onChange, currentWindow, getWindowForValue, scheduleWindowShift]
 );
